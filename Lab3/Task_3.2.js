619afbfdedc7dcc7ea3fc6050e8671f45d2df108
Adrew28 <j22038130@student.newinti.edu.my>
2024-09-20 14:43:24 +0800

    Add files via upload

--- Lab3/Task_3.2.js
+++ Lab3/Task_3.2.js
@@ -1,6 +1,7 @@
+//  Define chart width and height
 var w = 600;
 var h = 400;
-var padding = 40;
+var padding = 40;   //  Padding around the chart
 
 var dataset = [
     [2,8],
@@ -14,17 +15,19 @@ var dataset = [
     [5,12],
     [6,16],
     ];
-  
+
+//  Define the xScale using D3's scaleLinear
 var xScale = d3.scaleLinear()
     .domain([d3.min(dataset, function(d) {
             return d[0];
         }) - 2,
         d3.max(dataset, function(d) {
             return d[0];
-        }) + 5
+        }) + 5  //  Add extra padding
     ])
-    .range([padding, w - padding]);
+    .range([padding, w - padding]); //  Map ro chart width with padding
 
+//  Define the yScale using D3's scaleLinear 
 var yScale = d3.scaleLinear()
     .domain([d3.min(dataset, function(d) {
             return d[1];
@@ -35,42 +38,46 @@ var yScale = d3.scaleLinear()
     ])
     .range([h - padding, padding]);  // Inverted range
 
+// Create an SVG element inside the body (or a div with specific ID)
 var svg = d3.select("body")
     .append("svg")
-    .attr("width", w)
-    .attr("height", h);
+    .attr("width", w)   //  Set the width of the SVG
+    .attr("height", h); //  Set the height of the SVG
 
+// Define and create the x-axis with ticks and the xScale
 var xAxis = d3.axisBottom()
             .ticks(5)
             .scale(xScale);
                 
 svg.append("g")
-    .attr("transform", "translate(0, "+ (h - padding) +")")
+    .attr("transform", "translate(0, "+ (h - padding) +")") // Position the axis
     .call(xAxis);
 
+// Define and create the y-axis with ticks and the yScale
 var yAxis = d3.axisLeft()
             .ticks(5)
             .scale(yScale);
 
 svg.append("g")
-    .attr("transform", "translate(" + padding + ",0)")
+    .attr("transform", "translate(" + padding + ",0)")  // Position the axis
     .call(yAxis);
 
 // X-axis label
 svg.append("text")
-    .attr("text-anchor", "end")
-    .attr("x", w / 2)
-    .attr("y", h - 5)
-    .text("Tree Age (year)");
+    .attr("text-anchor", "end") // Align text at the end
+    .attr("x", w / 2)           // Center the label horizontally
+    .attr("y", h - 5)           // Position near the bottom of the SVG
+    .text("Tree Age (year)");   // Text for x-axis
 
 // Y-axis label
 svg.append("text")
     .attr("text-anchor", "end")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 15)
-    .attr("x", -h / 2)
-    .text("Tree Height (cm)");
+    .attr("transform", "rotate(-90)")   // Rotate the label for vertical positioning
+    .attr("y", 15)                      // Adjust vertical position of the label
+    .attr("x", -h / 2)                  // Center the label on the y-axis
+    .text("Tree Height (cm)");          // Text for y-axis
 
+// Create circle elements for each data point in the dataset
 svg.selectAll("circle")
     .data(dataset)
     .enter()
@@ -90,7 +97,7 @@ svg.selectAll("circle")
         }
         });
                     
-
+// Add text labels to each data point (x, y) above the circles
 svg.selectAll(".text")
     .data(dataset)
     .enter()
