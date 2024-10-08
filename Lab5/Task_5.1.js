a7fbc29069fb23d3c046bd1d2f8f25749ca8ffff
Adrew28 <j22038130@student.newinti.edu.my>
2024-10-08 08:22:40 +0800

    Add files via upload

--- Lab5/Task_5.1.js
+++ Lab5/Task_5.1.js
@@ -6,14 +6,16 @@ function init() {
     
     var dataset = [24,10,29,19,8,15,25,12,9,6,21,28];
     
+     // Scale for the x-axis (ordinal, based on the number of bars)
     var xScale = d3.scaleBand()
-                   .domain(d3.range(dataset.length))
-                   .rangeRound([0, w])
-                   .paddingInner(0.05);
+                   .domain(d3.range(dataset.length))    
+                   .rangeRound([0, w])  // Map the dataset to the width of the SVG
+                   .paddingInner(0.05); // Add padding between bars
     
+    // Scale for the y-axis (linear, based on the dataset values)
     var yScale = d3.scaleLinear()
                    .domain([0, d3.max(dataset)])
-                   .range([0, h]);
+                   .range([0, h]);  // Maps values to the height of the SVG
     
     var svg = d3.select("#chart")
                 .append("svg")
@@ -29,15 +31,15 @@ function init() {
             // Generate a new random dataset
             dataset = [];
             for (var i = 0; i < numValues; i++) {
-                var newNumber = Math.floor(Math.random() * maxValue);
+                var newNumber = Math.floor(Math.random() * maxValue);   // Generate random Value
                 dataset.push(newNumber); 
             }
     
             // Update the bars
             svg.selectAll("rect")
-               .data(dataset)
+               .data(dataset)   // Bind new data to the existing bars
                .attr("x", function(d, i) { 
-                   return xScale(i); 
+                   return xScale(i);    // Recalculate the x position for each bar
                })
                .attr("y", function(d) {
                    return h - yScale(d);  
@@ -47,14 +49,14 @@ function init() {
                    return yScale(d);                 
                })
                .attr("fill", function(d) {
-                   return "rgb(" + (d * 10) + "," + (255 - d * 10) + ", 100)";
+                   return "rgb(" + (d * 10) + "," + (255 - d * 10) + ", 100)";  // Update color based on data
                });
     
             // Update the text labels
             svg.selectAll("text")
                .data(dataset)
                .text(function(d){
-                   return d;
+                   return d;    // Display the new value
                })
                .attr("x", function(d, i){
                    return xScale(i) + xScale.bandwidth() / 2;
@@ -80,7 +82,7 @@ function init() {
            return yScale(d);
        })
        .attr("fill", function(d) {
-           return "rgb(" + (d * 10) + "," + (255 - d * 10) + ", 100)";
+           return "rgb(" + (d * 10) + "," + (255 - d * 10) + ", 100)";  // Set color based on the value
        });
     
     // Initial text labels creation
@@ -89,7 +91,7 @@ function init() {
        .enter()
        .append("text")
        .text(function(d) {
-           return d;
+           return d;    // Display the value of the data point
        })
        .attr("x", function(d, i) {
            return xScale(i) + xScale.bandwidth() / 2;
@@ -97,8 +99,8 @@ function init() {
        .attr("y", function(d) {
            return h - yScale(d) + 15;
        })
-       .attr("fill", "black")
-       .attr("text-anchor", "middle");
+       .attr("fill", "black")   // Set the text color
+       .attr("text-anchor", "middle");  // Center the text within each bar
     
 }
 
--- Lab5/Task_5.2.js
+++ Lab5/Task_5.2.js
@@ -7,13 +7,13 @@ function init() {
     var dataset = [24,10,29,19,8,15,25,12,9,6,21,28];
     
     var xScale = d3.scaleBand()
-                   .domain(d3.range(dataset.length))
-                   .rangeRound([0, w])
-                   .paddingInner(0.05);
+                   .domain(d3.range(dataset.length))  // Range based on the number of data points
+                   .rangeRound([0, w])               // Scale range spans the entire chart width
+                   .paddingInner(0.05);              // Add padding between bars
     
     var yScale = d3.scaleLinear()
-                   .domain([0, d3.max(dataset)])
-                   .range([0, h]);
+                   .domain([0, d3.max(dataset)])    // Scale domain is from 0 to max data value
+                   .range([0, h]);                  // Scale output is the height of the chart
     
     var svg = d3.select("#chart")
                 .append("svg")
@@ -23,8 +23,8 @@ function init() {
     // Button click event to generate new data and update chart
     d3.select("#update")
         .on("click", function() {
-            var numValues = dataset.length;
-            var maxValue = 25;
+            var numValues = dataset.length; // Keep track of the number of values
+            var maxValue = 25;               // Maximum value for random data generation
     
             // Generate a new random dataset
             dataset = [];
@@ -40,7 +40,7 @@ function init() {
                .delay(function(d, i){
                 return i * 100;
                })
-               .duration(1000)
+               .duration(1000)  // Duration of the transistion
                .ease(d3.easeCubicInOut)
                .attr("x", function(d, i) { 
                    return xScale(i); 
